b12c96212ce2be7091e40c3a2b02f5b6596dfcd0
DevJWN <dev.jwn@gmail.com>
2020-11-20 05:54:17 +0800

    Module name sorting

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,44 @@
 import { IImport, IParserOptions } from "import-sort-parser";
-import { IMatcherFunction, IStyleAPI, IStyleItem } from "import-sort-style";
+import { IMatcherFunction, ISorterFunction, IStyleAPI, IStyleItem } from "import-sort-style";
 
 const defaultGroups = [["^@\\w", "^\\w"], ["^\\.\\./", "^\\./"], ["\\.s?css$"]];
 
-export default function Index(
-  styleApi: IStyleAPI,
-  _file: string,
-  options?: IParserOptions & { groups: string[][] }
-): IStyleItem[] {
+const sortByMemberType: ISorterFunction = (i1: IImport, i2: IImport) => {
+  const compareHasNamespaceMember = Number(!!i1.namespaceMember) - Number(!!i2.namespaceMember);
+  if (compareHasNamespaceMember != 0) {
+    return compareHasNamespaceMember;
+  }
+
+  const compareHasDefaultMember = Number(!!i1.defaultMember) - Number(!!i2.defaultMember);
+  if (compareHasDefaultMember != 0) {
+    return compareHasDefaultMember;
+  }
+
+  const compareHasNamedMembers = Number(i1.namedMembers.length > 0) - Number(i2.namedMembers.length > 0);
+  return compareHasNamedMembers;
+};
+
+const Index = (styleApi: IStyleAPI, _file: string, options?: IParserOptions & { groups: string[][] }): IStyleItem[] => {
   const groups = options && options.groups ? options.groups : defaultGroups;
 
-  const {
-    and,
-    member,
-    name,
-    unicode,
-    hasNoMember,
-    hasDefaultMember,
-    hasNamespaceMember,
-    hasNamedMembers,
-    isAbsoluteModule,
-    isRelativeModule,
-  } = styleApi;
+  const { and, unicode, moduleName, member, name, hasNoMember, isAbsoluteModule, isRelativeModule } = styleApi;
 
-  return groups.flatMap((group) => {
-    return [
-      { match: and(hasNoMember, isAbsoluteModule) },
-      { separator: true },
-      { match: and(hasNoMember, isRelativeModule) },
-      { separator: true },
-      ...group.flatMap((regex) => {
-        const match: IMatcherFunction = (imported: IImport) => Boolean(new RegExp(regex).exec(imported.moduleName));
+  return [
+    { match: and(hasNoMember, isAbsoluteModule), separator: true },
+    { match: and(hasNoMember, isRelativeModule), separator: true },
+    ...groups.flatMap((group) => [
+      ...group.map((regex) => {
+        const match: IMatcherFunction = (i: IImport) => Boolean(new RegExp(regex).exec(i.moduleName));
 
-        return [
-          { match: and(match, hasNamespaceMember), sort: member(unicode), sortNamedMembers: name(unicode) },
-          { match: and(match, hasDefaultMember), sort: member(unicode), sortNamedMembers: name(unicode) },
-          { match: and(match, hasNamedMembers), sort: member(unicode), sortNamedMembers: name(unicode) },
-        ];
+        return {
+          match,
+          sort: [sortByMemberType, moduleName(unicode), member(unicode)],
+          sortNamedMembers: name(unicode),
+        };
       }),
       { separator: true },
-    ];
-  });
-}
+    ]),
+  ];
+};
+
+export default Index;
